37577c75f1976bcd41ce2f80623e1fbc90c32c39
RonaldSory <165039163+RonaldSory@users.noreply.github.com>
2024-04-03 23:39:19 +0000

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "🧛𝘿𝙍𝘼𝘾𝙐𝙇𝘼🧛\n\n 𝐏𝐑𝐄𝐅𝐈𝐗 ====> |%1|\n_______________________\n 𝙊𝙒𝙉𝙀𝙍 : 𝘾𝙄 🇨🇮"
+			myPrefix: "🌹 𝐑𝐨𝐧𝐚𝐥𝐝 🌹\n\n voici mon 𝐏𝐑𝐄𝐅𝐈𝐗 ====> |%1|\n_______________________\n 🌹📬"
 		}
 	},
 
